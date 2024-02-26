acd53bbf78347fe8a43817730979cddf7d56513b
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2024-02-27 00:46:01 +0530

    first commit

---
+++ lengthLastWord.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class lengthLastWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        String arr[] = s.split(" ");
+        int x = arr.length - 1;
+
+        String arr1 = arr[x];
+
+        int y = arr1.length();
+        System.out.println(y);
+
+        sc.close();
+    }
+}
---
+++ removeElement.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class removeElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int nums[] = new int[0];
+        int val = sc.nextInt();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+
+        sc.close();
+    }
+}
