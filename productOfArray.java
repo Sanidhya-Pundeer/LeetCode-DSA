a7de563292b05bf60bbd876cf652fe6f937fa3e0
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2024-04-15 23:33:06 +0530

    productOfArray in process

---
+++ productOfArray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class productOfArray {
+    public static void main(String[] args) {
+        float arr[] = new float[] { 1, 2, 3, 4 };
+        int n = arr.length;
+        float result[] = new float[n];
+        float temp = 1;
+        for (int i = 0; i < n; i++) {
+            temp = temp * arr[i];
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[i] = temp / arr[i];
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
