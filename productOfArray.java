091e050c0c3f002a170eb79e5f8ff527357ddc38
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2024-04-19 15:35:27 +0530

    product of Array comleted

--- productOfArray.java
+++ productOfArray.java
@@ -2,19 +2,38 @@ import java.util.Arrays;
 
 public class productOfArray {
     public static void main(String[] args) {
-        float arr[] = new float[] {};
+        int arr[] = new int[] { 1, 2, 3, 4 };
         int n = arr.length;
-        float result[] = new float[n];
-        float temp = 1;
+        int left[] = new int[n];
+        int right[] = new int[n];
+        int product = 1;
+
         for (int i = 0; i < n; i++) {
-            temp = temp * arr[i];
+            if (i == 0) {
+                left[0] = 1;
+            } else {
+                left[i] = product;
+                product = product * arr[i];
+            }
+
         }
-        for (int i = 0; i < n; i++) {
-            if (arr[i] != 0) {
-                arr[i] = temp / arr[i];
+        product = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i == n - 1) {
+                right[n - 1] = 1;
+                product = product * arr[n - 1];
+            } else {
+                right[i] = product;
+                product = product * arr[i];
             }
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(left));
+        System.out.println(Arrays.toString(right));
 
+        for (int i = 0; i < n; i++) {
+            arr[i] = left[i] * right[i];
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
