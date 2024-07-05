c9e45792b57fc35058a4cbc8d41aab3a433bec77
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2024-07-05 10:44:53 +0530

    palindrome

--- lengthLastWord.java
+++ lengthLastWord.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class lengthLastWord {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
@@ -16,7 +17,3 @@ public class lengthLastWord {
         sc.close();
     }
 }
-// testing
-// riya code
-// hemloooo
-// asjbgfasibgiueabsignewiongiowe
