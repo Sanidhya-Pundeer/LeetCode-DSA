ca8968ad529ac42d46bbde56f12f484e65672002
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2024-07-10 01:48:52 +0530

    contains duplicate and reverse words done

--- containsDuplicate.java
+++ containsDuplicate.java
@@ -1,14 +1,16 @@
+import java.util.Arrays;
+
 public class containsDuplicate {
     public static void main(String[] args) {
-        int nums[] = new int[] { 1, 2, 3, 3 };
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    System.out.println("true");
-                }
-            }
-            System.out.println("false");
+        int[] nums = new int[] { 4, 5, 1, 3, 1 };
+        Arrays.sort(nums);
+        boolean flag = true;
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                flag = true;
         }
+        flag = false;
+        System.out.println(flag);
     }
 }
